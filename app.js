5a09cfb824a18eafe9737d59ae284ae27d5aedd6
FeriW02 <full.stack020224@gmail.com>
2024-11-29 03:56:43 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,180 @@
+// Task-01
+
+// console.log("My Name Is Ferid");
+
+// Task-02
+
+// const name = "Ferid";
+
+// console.log(name);
+
+// Task-03
+
+// 2user--
+// c@rt--
+// person++ 
+// title color--
+// console--
+// $add++
+
+// Task-04
+
+// let bookPrice = 750;
+// let bookAmount = 14;
+
+// console.log(bookAmount*bookPrice);
+
+// Task-05
+
+// let kmhSpeed = 75;
+// let msSpeed = 3.6;
+// console.log(kmhSpeed/msSpeed);
+
+// Task-06
+
+// ???????
+
+// Task-07
+
+// let number = 24;
+
+// let powerOfnumber = Math.pow(num, 5);
+
+// let divideOfnumber = num % 3;
+
+// console.log("ədədin beşinci dərəcə qüvvətə qaldırılmış vəziyyətini: ", powerOfnumber);
+// console.log("3-ə bölünmədən qalan bu ədədin qalığını: ", divideOfnumber);
+
+// Task-08
+
+// let name = "Ferid"
+// let lastName = "Teyubov"
+
+// console.log(`Sizin adınız ${name} ${lastName}. Tanışlığımıza çox şadam!`);
+
+// Task-09
+
+// let name = "Ferid"
+// let lastName = "Teyubov"
+
+// let fullName = `Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}`
+
+// console.log(fullName);
+
+// Task-10
+
+// let fullName = "Ferid Teyubov"
+
+// console.log(fullName.length-1);
+
+// Task-11
+
+// let fullName = "Ferid Teyubov"
+// let result = fullName[0] + fullName[fullName.indexOf(" ") + 1]
+
+// console.log(result);
+
+// Task-12
+
+// let fullName = prompt("Adınızı daxil edin!")
+// let [firstName, lastName] = fullName.split(" ")
+
+// console.log(lastName, firstName);
+
+// Task-13
+
+// let userName = prompt("Adınızı daxil edin!")
+
+// console.log(`Salam, ${userName}!`);
+
+// Task-14
+
+// let firstNumber = prompt("Reqem daxil edin")
+// let secondNumber = prompt("Reqem daxil edin")
+
+// console.log("Toplama:", firstNumber + secondNumber);
+// console.log("Vurma:", firstNumber * secondNumber);
+// console.log("Bölme:", firstNumber / secondNumber);
+// console.log("Çıxma:", firstNumber - secondNumber);
+
+// Task-15
+
+// let impact = 12345 * 98765 
+// let result = impact.toString()[5];
+// console.log(result);
+
+// Task-16
+
+// "" + 1 + 0 = "10"
+
+// "" - 1 + 0 = -1
+
+// "true" - false = NaN
+
+// 6 / "3" = 2
+
+// 4 + 5 + "px" = "9px"
+
+// "$" + 4 + 5 = "$45"
+
+// "4" - 2 = 2
+
+// NaN + "px" = "NaNpx"
+
+// 7 / 0 = Infinity
+
+// "-9" + 5 = "-95"
+
+// "-9" - 5 = -14
+
+// null + 1 = 1
+
+// undefined + 1 = NaN
+
+// Task-17
+
+// let number = prompt("Dörd rəqəmli ədəd daxil edin!");
+// let sum = 0;
+
+// for (let oneByOne of number) {
+//   sum += Number(oneByOne); 
+// }
+
+// console.log(sum);
+
+// Task-18
+
+// let fullName = prompt("Adınızı daxil edin!")
+// let onlyName = fullName.split(" ")[0];
+
+// console.log(onlyName);
+
+// Task-19
+
+// let price = prompt("Qiyməti daxil edin {59.23}:");
+
+// let [manat, qepik] = price.split(".");
+
+// console.log(`${manat}manat ${qepik}qəpik`);
+
+// Task-20
+
+// let salary = +prompt("Əmək haqqınızı daxil edin:");
+// let tax = salary * 0.13;
+
+// let resultSalary = salary - tax;
+
+// console.log(`Əmək haqqınız: ${resultSalary} AZN`);
+
+// Task-21
+
+// let fullName = prompt("Zəhmət olmasa tam adınızı və atanızın adını daxil edin.!")
+
+// let [lastName, firstName, fatherName] = fullName.split(" ")
+
+// console.log(`${lastName} ${firstName[0]}.${fatherName[0]}`);
+
+
+// Task-22
+
+// ??????
